dddadeb77240ed5025bd7351e7bb007a90672fa3
Morswin <41349347+Morswin@users.noreply.github.com>
2023-09-26 23:13:58 +0200

    Leaving the project as is
    Because apparently I don't understand how to import a TS type from a
    separate file.
    Maybe I'll fix this sometime in the future.
    Maybe not.

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+    // import Post from './../types';
+
     const userStore = useUserStore();
+    const new_post_title = ref<string>("Title");
+    const new_post_content = ref<string>("Content content");
+    const create_new_post = (_title: string, _content: string) => {
+        // userStore.
+    }
 </script>
 
 <template>
@@ -12,9 +19,9 @@
                 action="" 
                 class="flex flex-col ml-2 w-1/2 items-center border-2 border-black rounded-md p-2"
             >
-                <div class="flex justify-between w-5/6">
+                <div class="flex justify-between w-full">
                     <label for="post_title">Title:</label>
-                    <input type="text" id="post_title">
+                    <input type="text" id="post_title" v-model="new_post_title" class="pl-1">
                 </div>
                 <div>
                     <label for="">Content:</label>
@@ -23,12 +30,13 @@
                         id="post_content" 
                         cols="30" 
                         rows="10"
-                        class="border-2 border-black rounded-md h-20"
+                        class="border-2 border-black rounded-md h-20 pl-1 w-full text-xs"
+                        v-model="new_post_content"
                     >
                     </textarea>
                 </div>
                 <button 
-                    @click.prevent=""
+                    @click.prevent="create_new_post"
                     class="mt-2 px-1 border-2 border-black rounded-md w-16"
                 >
                     Post
--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-export interface Test {
+export interface Post {
     title: string
     content: string
     image?: string
